e0d1691dbb36c1140b2bb2c3bb62b7f6b90b9460
RaiAlbani <muhammadraihansyahalbani@gmail.com>
2025-12-27 23:00:26 +0700

    Add files via upload

---
+++ tabelipk.sql
@@ -0,0 +1,160 @@
+-- tabel: ipk
+-- schema: tabeldb
+-- database: belajardb
+
+-- tabel ipk
+--- nim INTEGER
+--- semester VARCHAR(3)
+--- ipk FLOAT
+
+CREATE TABLE tabeldb.ipk (
+	nim INTEGER,
+	semester VARCHAR(3),
+	ipk FLOAT
+);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S1', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S2', 3.7);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S3', 3.92);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S4', 3.75);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S5', 3.55);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S6', 3.8);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S7', 3.15);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100190, 'S8', 2.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S1', 3.5);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S2', 3.63);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S3', 3.7);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S4', 3.68);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S5', 3.3);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S6', 3.55);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S7', 3.25);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100191, 'S8', 3.1);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S1', 3.2);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S2', 3.85);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S3', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S4', 3.78);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S5', 3.8);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S6', 3.3);
+
+INSERT INTO tabeldb.ipK (nim, semester, ipk)
+VALUES (100192, 'S7', 3.55);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100192, 'S8', 3.4);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S1', 3.8);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S2', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S3', 3.82);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S4', 3.76);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S5', 3.6);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S6', 3.8);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S7', 4.0);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100193, 'S8', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S1', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S2', 3.5);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S3', 3.4);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S4', 3.6);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S5', 3.85);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S6', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S7', 3.8);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100194, 'S8', 4.00);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S1', 4.00);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S2', 3.9);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S3', 3.5);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S4', 3.45);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S5', 3.6);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S6', 4.00);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S7', 3.7);
+
+INSERT INTO tabeldb.ipk (nim, semester, ipk)
+VALUES (100195, 'S8', 3.85);
+
+SELECT * FROM tabeldb.ipk;
